e8e5e7cc4f06fc569833d944b4e12d3d44a70771
Safeer Motiwala <safeer@unknownee237c2dec86.attlocal.net>
2024-12-29 01:56:59 -0600

    CLI Greeting

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        while(true) {
+            System.out.println("""
+                    What would you like to do today?
+                    1) Employee Sign In
+                    2) Admin Sign In
+                    3) Exit System
+                    """);
+            String userInput = scanner.nextLine();
+            switch (userInput) {
+                case "1" -> { System.out.println("1"); break; }
+                case "2" -> { System.out.println("2"); break; }
+                case "3" -> { System.exit(64); break; }
+                default -> { System.out.print("Invalid choice. Please try again.\n" ); }
+
+            }
+        }
     }
 }
